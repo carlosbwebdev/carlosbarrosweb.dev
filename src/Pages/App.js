0b304c50fbc435aa88a9bd3a046db7d1c66f6d88
Carlos Barros <carlosbwebdev@gmail.com>
2022-10-31 14:42:35 +0000

    files system structure

---
+++ src/Pages/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from '../Styles/Pages/App.module.css';
+
+function App() {
+  return (
+    <div>
+      <section className={styles.sectionApp}>
+        <h1>Hello</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
